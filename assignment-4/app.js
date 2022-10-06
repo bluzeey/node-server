eae09246e6ea1ecf1a5db0c9365b450e6bf0a998
Sahil Maheshwari <sahilm1711@gmail.com>
2022-10-07 00:24:30 +0530

    Adding some files

--- assignment-4/app.js
+++ assignment-4/app.js
@@ -1,14 +1,23 @@
 const express=require('express')
 let app=express()
 
+const users=[]
 
+app.set('view engine','ejs')
+app.set('views','views')
 
-app.get('/',(req,res,next)=>{
-   res.send('This template is working!')
+app.post('/',(req,res)=>{
+   users.push(req.body.user)
+   res.redirect('/users')
 })
 
-app.get('/users',(req,res,next)=>{
-    res.send('This is the users on our page.')
+app.get('/',(res)=>{
+   res.render('index', {pageTitle:'Home Page'})
+})
+
+
+app.get('/users',(res)=>{
+    res.render('users', {pageTitle:'Users Page', users:users})
 })
 
 
