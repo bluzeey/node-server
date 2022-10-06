8e252b9aa631d243de249838e2e1fc41b95b7d64
Sahil Maheshwari <sahilm1711@gmail.com>
2022-10-07 01:01:28 +0530

    Adding ejs assignment

--- assignment-4/app.js
+++ assignment-4/app.js
@@ -1,24 +1,29 @@
 const express=require('express')
+
+const bodyParser=require('body-parser')
 let app=express()
 
-const users=[]
+let users=[]
+
 
 app.set('view engine','ejs')
 app.set('views','views')
+
+app.use(express.urlencoded({extended: true}))
+
+app.get('/users',(req,res)=>{
+    res.render('users', {pageTitle:'Users Page', userList:users});
+})
+
+app.get('/',(req,res)=>{
+    res.render('index', {pageTitle:'Home Page'});
+})
 
 app.post('/',(req,res)=>{
    users.push(req.body.user)
    res.redirect('/users')
 })
 
-app.get('/',(res)=>{
-   res.render('index', {pageTitle:'Home Page'})
-})
-
-
-app.get('/users',(res)=>{
-    res.render('users', {pageTitle:'Users Page', users:users})
-})
 
 
 app.listen(3000)
