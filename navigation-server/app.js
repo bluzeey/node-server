319009d7a272ea91b6d7f118cee0840ea485407d
Sahil Maheshwari <sahilm1711@gmail.com>
2022-10-08 16:32:52 +0530

    Adding files

--- navigation-server/app.js
+++ navigation-server/app.js
@@ -1,17 +1,24 @@
-const express=require('express');
+const path = require('path');
 
-const app=express();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const userRoutes=require('./routes/user')
+const errorController = require('./controllers/error');
 
-app.use(userRoutes)
+const app = express();
 
-app.get(('/'),(req,res)=>{
-    res.send('Welcome to the homepage!')
-})
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use((req,res)=>{
-    res.status(404).send('<h1>Page not found </h1>')
-})
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.listen(3000)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
