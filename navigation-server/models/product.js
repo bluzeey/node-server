edb2f81e918f1a6095d4b76a60659ba15e29e0c4
Sahil Maheshwari <sahilm1711@gmail.com>
2022-10-09 11:05:53 +0530

    Update product.js

--- navigation-server/models/product.js
+++ navigation-server/models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -46,16 +48,16 @@ module.exports = class Product {
       }
     });
   }
-  
-  static deleteById(id){
+
+  static deleteById(id) {
     getProductsFromFile(products => {
-      const updatedProducts = products.filter(p => p.id !== id);
-      fs.writeFile(p,JSON.stringify(UpdatedProducts), err =>{
-        if(!err){
-          
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
         }
-        console.log(err)
-      })
+      });
     });
   }
 
