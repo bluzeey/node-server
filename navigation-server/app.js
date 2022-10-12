428c9f25fe369f15074961b46b780d998844a5f2
Sahil Maheshwari <sahilm1711@gmail.com>
2022-10-12 13:32:52 +0530

    Adding files for mongodb extension

--- navigation-server/app.js
+++ navigation-server/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
+const User = require('./models/user');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,9 +16,17 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.use((req, res, next) => {
+//   User.findById(1)
+//     .then(user => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
---
+++ navigation-server/util/database.js
@@ -0,0 +1,4 @@
+const mongodb=require('mongodb');
+const MongoClient=mongodb.MongoClient;
+
+MongoClient.connect('mongodb+srv://sahilm:sahilm123@mongodb-tut.stjbs.mongodb.net/test')
