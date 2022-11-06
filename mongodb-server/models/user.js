4c4b03da076d4b80a60d79858b6d079df705c436
Sahil Maheshwari <sahilm1711@gmail.com>
2022-11-06 16:29:18 +0530

    creating function using mongodb

--- mongodb-server/models/user.js
+++ mongodb-server/models/user.js
@@ -1,16 +1,31 @@
-const Sequelize = require('sequelize');
+const mongodb=require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
+class User{
+  constructor(username,email){
+    this.name=username;
+    this.email=email
+  }
+  save(){
+    const db=getDb()
+    return db.collection('users').insertOne(this)
+  }
+  
+  addToCart(product){
+    const cartProduct=this.cart.items.findIndex(cp=>{
+      return cp._id===product._id
+    })
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  }
 
+  static findById(userId){
+    const db=getDb();
+    return db.collection('users').find({_id:new mongodb.ObjectId(userId)}).next().then(
+      user=>{
+        console.log(user)
+        return user
+      }
+    ).catch(err=>console.log(err))
+  }
+}
 module.exports = User;
